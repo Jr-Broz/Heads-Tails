777a7f8401323056dbd8c37c0351b27ef25fa934
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-07-31 08:58:21 -0300

    Created archive.cs

---
+++ archive.cs
@@ -0,0 +1,23 @@
+	using System;
+					
+	public class Program
+{
+	public static void Main()
+	
+	{
+
+	Random coin = new Random();
+	int flip = coin.Next(0,2);
+		
+	
+		if(flip == 0){
+			
+		Console.WriteLine("Cara", "Head");
+		
+		}else{
+			
+			Console.WriteLine("Coroa", "Tails");
+		
+		}	
+	}
+}
